5bb1661780ae66afbc5227f9398d6782eaa6a7a2
haoqic <39285061+OreoDust@users.noreply.github.com>
2020-04-12 17:32:33 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var http = require("http");
+var port = process.env.port || 5000;
 
 http.createServer(function (request, response) {
    // Send the HTTP header 
@@ -8,7 +9,7 @@ http.createServer(function (request, response) {
    
    // Send the response body as "Hello World"
    response.end('Hello World\n');
-}).listen(8081);
+}).listen(port);
 
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
