8958f4cdebee316c3d62cda729b69a78b03ddd8f
haoqic <39285061+OreoDust@users.noreply.github.com>
2020-04-15 23:40:11 +1000

    Update app.js

--- app.js
+++ app.js
@@ -3,26 +3,6 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-/**** DATABASE ****/
-// import mongoose module
-const mongoose = require('mongoose');
-var MongoClient = require('mongodb').MongoClient;
-// set up default mongoose connection
-const mongoDB = 'mongodb+srv://haoqic:8717192@incubeta-wowel.mongodb.net/test?retryWrites=true&w=majority';
-//mongoose.connect(mongoDB);
-MongoClient.connect(mongoDB, { useNewUrlParser: true }, function(err, db) {
-  if (err) throw err;
-  console.log("DataBase Created");
-  db.close();
-});
-
-// 让 mongoose 使用全局 Promise 库
-mongoose.Promise = global.Promise;
-// 取得默认连接
-const db = mongoose.connection;
-// 将连接与错误事件绑定（以获得连接错误的提示）
-db.on('error', console.error.bind(console, 'ERROR: Connect MangoDB Failed'));
-/**** DATABASE ****/
 
 app.use(express.static('public'));
 
