f6fe7d9a948dc63a237edbca3a233db339105c69
haoqic <39285061+OreoDust@users.noreply.github.com>
2020-04-17 01:05:09 +1000

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+var homeRouter = require('./routes/home');  //Import routes for "home" area of site
 
 var compression = require('compression');
 var helmet = require('helmet');
@@ -37,8 +36,7 @@ app.use(compression()); // Compress all routes
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
+app.use('/home', homeRouter);  // Add home routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
