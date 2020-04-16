bdd746e7d85a3607acd5826231882e9411854f6a
haoqic <39285061+OreoDust@users.noreply.github.com>
2020-04-17 00:59:23 +1000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-// GET home page.
-router.get('/', function(req, res) {
-  res.redirect('/home');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'InCuBeTa' });
 });
 
 module.exports = router;
