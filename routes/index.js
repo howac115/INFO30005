c3f6a499d97725c2f9b4286e0af37aabb1fc00bb
haoqic <39285061+OreoDust@users.noreply.github.com>
2020-04-18 00:19:04 +1000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-// GET home page.
+// Home page.
 router.get('/', function(req, res) {
   res.redirect('/home');
 });
+// Dashboard page.
+router.get('/dashboard', (req, res) => res.render('dashboard'));
 
 module.exports = router;
