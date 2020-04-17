49b9bf881ac32c4d57e281836a89994725745325
haoqic <39285061+OreoDust@users.noreply.github.com>
2020-04-17 16:22:20 +1000

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/login', forwardAuthenticated, (req, res) => res.render('login'));
+router.get('/', function(req, res, next) {
+  res.render('login', { title: 'InCuBeTa' });
+});
 
 module.exports = router;
