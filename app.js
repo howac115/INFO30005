41ff5a7aac5757aaaea1e6e868f1f8e619034b34
haoqic <39285061+OreoDust@users.noreply.github.com>
2020-04-17 16:28:06 +1000

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var homeRouter = require('./routes/home');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/home', homeRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
