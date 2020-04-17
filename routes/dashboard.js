1d81c1764efc609ebd93092ab3685cc2528d7371
haoqic <39285061+OreoDust@users.noreply.github.com>
2020-04-18 00:18:26 +1000

    Delete dashboard.js

--- routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET job listing page. */
-router.get('/', function(req, res, next) {
-  res.render('dashboard', { title: 'InCuBeTa' });
-});
-
-module.exports = router;
