fad21712a5fc0c5b205ae61b7fc49f888b73f35f
haoqic <39285061+OreoDust@users.noreply.github.com>
2020-04-18 14:05:01 +1000

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,15 +1,8 @@
 module.exports = {
-    ensureAuthenticated: function(req, res, next) {
-      if (req.isAuthenticated()) {
-        return next();
-      }
-      res.redirect('/home/login');
-    },
-    forwardAuthenticated: function(req, res, next) {
-      if (!req.isAuthenticated()) {
-        return next();
-      }
-      res.redirect('/dashboard');      
+  ensureAuthenticated: function(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
     }
-  };
- 
+    res.redirect('/home/login');
+  }
+}
