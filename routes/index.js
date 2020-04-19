c296a1396c3c2c45963ffb38bd75fe980528e3ee
haoqic <39285061+OreoDust@users.noreply.github.com>
2020-04-20 00:43:28 +1000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var { ensureAuthenticated } = require('../config/auth');
 
-// Home page.
+// Index page.
 router.get('/', (req, res) => res.render('index'));
-
-// Dashboard page.
-router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dashboard'));
 
 module.exports = router;
