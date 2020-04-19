1b1ef8c9718b6724efef711f8e1e0f5e1cc63c5a
haoqic <39285061+OreoDust@users.noreply.github.com>
2020-04-20 00:41:39 +1000

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,7 +1,13 @@
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  name: {
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  first_name: {
+    type: String,
+    required: true
+  },
+  family_name: {
     type: String,
     required: true
   },
@@ -19,6 +25,23 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-const User = mongoose.model('User', UserSchema);
+// Virtual for user "full" name.
+UserSchema.virtual('name').get(function() {
+  var fullname = '';
 
-module.exports = User;
+  if (this.first_name && this.family_name) {
+    fullname = this.family_name + ', ' + this.first_name;
+  }
+
+  if (!this.first_name && !this.family_name) {
+    fullname = '';
+  }
+  return fullname;
+});
+
+// Virtual for user instance URL.
+UserSchema.virtual('url').get(function() {
+  return '/dashboard/user/'+this._id;
+});
+
+module.exports = mongoose.model('User', UserSchema);
