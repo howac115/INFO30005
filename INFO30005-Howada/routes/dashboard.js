4f39abfd0e238e2c35a6ee0ce824b12dc601b315
Skylar-Yang <61859437+Skylar-Yang@users.noreply.github.com>
2020-04-21 13:18:34 +1000

    Add files via upload

---
+++ INFO30005-Howada/routes/dashboard.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var router = express.Router();
+var { ensureAuthenticated } = require('../config/auth');
+
+// Controllers.
+var job_controller = require('../controllers/jobController');
+var tag_controller = require('../controllers/tagController');
+var user_controller = require('../controllers/userController');
+
+// GET request to Dashboard page. Protected from when logged out.
+router.get('/', ensureAuthenticated, function(req, res) {
+    res.render('dashboard', {
+        current_user: req.user
+    })
+});
+
+
+/// JOB ROUTES ///
+
+// GET request to view Job Create page.
+router.get('/job/create', job_controller.job_create_get);
+
+// POST request to create new Job
+router.post('/job/create', job_controller.job_create_post);
+
+// GET request to delete Job.
+router.get('/job/:id/delete', job_controller.job_delete_get);
+
+// POST request to delete Job.
+router.post('/job/:id/delete', job_controller.job_delete_post);
+
+// GET request to update Job
+router.get('/job/:id/update', job_controller.job_update_get);
+
+// POST request to update Job
+router.post('/job/:id/update', job_controller.job_update_post);
+
+// GET request for one Job
+router.get('/job/:id', job_controller.job_detail);
+
+// GET request for list of all Jobs.
+router.get('/jobs', job_controller.job_list);
+
+
+/// USER ROUTES ///
+
+// GET request to update user profile
+router.get('/user/:id/update', user_controller.user_update_get);
+
+// POST request to update user profile
+router.post('/user/:id/update', user_controller.user_update_post);
+
+// GET request for one user
+router.get('/user/:id', user_controller.user_detail);
+
+/// TAG ROUTES ///
+
+// GET request to view Tag Create page.
+router.get('/tag/create', tag_controller.tag_create_get);
+
+// POST request to create new Tag
+router.post('/tag/create', tag_controller.tag_create_post);
+
+// GET request to delete Genre.
+router.get('/tag/:id/delete', tag_controller.tag_delete_get);
+
+// POST request to delete Genre.
+router.post('/tag/:id/delete', tag_controller.tag_delete_post);
+
+// GET request to update Genre.
+router.get('/tag/:id/update', tag_controller.tag_update_get);
+
+// POST request to update Genre.
+router.post('/tag/:id/update', tag_controller.tag_update_post);
+
+// GET request for one Tag.
+router.get('/tag/:id', tag_controller.tag_detail);
+
+// GET request for list of all tags
+router.get('/tags', tag_controller.tag_list);
+
+module.exports = router;
---
+++ INFO30005-Howada/routes/home.js
@@ -0,0 +1,97 @@
+var express = require('express');
+var router = express.Router();
+var bcrypt = require('bcryptjs');
+var passport = require('passport');
+
+/* Load user model */
+var User = require('../models/user');
+var { forwardAuthenticated } = require('../config/auth');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('home', { 
+        title: 'InCuBeTa',
+    })
+});
+
+/* Login page */
+router.get('/login', (req, res) => res.render('login'));
+
+/* Register page */
+router.get('/register', (req, res) => res.render('register'));
+
+/* Register handle */
+router.post('/register', (req, res) => {
+  const { first_name, family_name, email, password, password2 } = req.body;
+  let errors = [];
+
+  if ( !first_name || !family_name || !email || !password || !password2 ) {
+    errors.push({ msg: 'Please enter all fields' });
+  }
+
+  if (password != password2) {
+    errors.push({ msg: 'Passwords do not match' });
+  }
+
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      first_name,
+      family_name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        errors.push({ msg: 'Email already exists' });
+        res.render('register', {
+          errors,
+          first_name,
+          family_name,
+          email,
+          password,
+          password2
+        });
+      } else {
+        const newUser = new User({
+          first_name,
+          family_name,
+          email,
+          password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => {
+                res.redirect('/home/login');
+              })
+              .catch(err => console.log(err));
+          });
+        }); 
+      }
+    });
+  }
+});
+
+
+/* Login handle */
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/home/login',
+  })(req, res, next);
+});
+
+/* Logout handle */
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/home/login');
+});
+
+module.exports = router;
