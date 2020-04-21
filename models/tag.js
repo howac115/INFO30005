dc2e5c479409a61ce7c69b8f6e3d552cdcd3ab7a
Xiancheng Yang <xiancheng@student.unimelb.edu.au>
2020-04-21 14:31:19 +1000

    master

---
+++ models/tag.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var TagSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 1, 
+        max: 100
+    }
+});
+
+// Virtual for this genre instance URL.
+TagSchema
+.virtual('url')
+.get(function () {
+  return '/dashboard/tag/'+this._id;
+});
+
+// Export model.
+module.exports = mongoose.model('Tag', TagSchema);
