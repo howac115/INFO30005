753990d96bc13134690a74c206d148a05aed8a16
Theresasa <leetj@student.unimelb.edu.au>
2020-05-15 14:21:49 +1000

    not much a bit of a mess, experimenting with burger menu

--- public/components/sideNav.js
+++ public/components/sideNav.js
@@ -1,6 +1,6 @@
 import { slide as Menu } from 'react-burger-menu'
 
-class Example extends React.Component {
+class sideNav extends React.Component {
     showSettings (event) {
         event.preventDefault();
     .
@@ -11,12 +11,23 @@ class Example extends React.Component {
     render () {
         // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
         return (
-            <Menu>
-                <a id="home" className="menu-item" href="/">Home</a>
-                <a id="about" className="menu-item" href="/about">About</a>
-                <a id="contact" className="menu-item" href="/contact">Contact</a>
-                <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-            </Menu>
+            // <Menu>
+            //     <a id="home" className="menu-item" href="/">Home</a>
+            //     <a id="about" className="menu-item" href="/about">About</a>
+            //     <a id="contact" className="menu-item" href="/contact">Contact</a>
+            //     <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+            // </Menu>
+            Menu
+                a#home(classname='menu-item', href='/') Home
+                a#about(classname='menu-item', href='/about') About
+                a#contact(classname='menu-item', href='/contact') Contact
+                a(onclick='{' this.showsettings='' }='' classname='menu-item--small' href='') Settings
+
+
+
         );
     }
 }
+
+const domContainer = document.querySelector('#side-menu');
+ReactDOM.render(e(sideNav), domContainer);
